868526fc4aa69b4f35970a865c48cfaf64fabdbb
Aryan Kumar <aryankumawat01@gmail.com>
2023-09-29 16:15:30 +0530

    Initial Commit

---
+++ android/app/src/main/java/com/example/real/real_estate/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.real.real_estate;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:real_estate/splash_services/splash_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Real Estate',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      initialRoute: SplashScreen.id,
+      routes: {
+        SplashScreen.id:(context) => const SplashScreen(),
+      },
+    );
+  }
+}
+
