fc9cf3d4ce7e11022a34dcf4be4858cbda2dea85
Aryan Kumar <aryankumawat01@gmail.com>
2023-09-29 18:47:22 +0530

    Splash Screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:real_estate/splash_services/splash_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Real Estate',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orangeAccent),
         useMaterial3: true,
       ),
       initialRoute: SplashScreen.id,
--- lib/splash_services/splash_screen.dart
+++ lib/splash_services/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class SplashScreen extends StatefulWidget {
   static const String id = "SplashScreen";
@@ -8,15 +9,55 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin{
+
+  AnimationController? _controller;
+  Animation<Color>? animation;
+
+  final colors = <TweenSequenceItem<Color>>[
+    TweenSequenceItem(tween: Tween<Color>(begin:Colors.red,end:Colors.blue),
+        weight: 1.0),
+    TweenSequenceItem(tween: Tween<Color>(begin: Colors.blue,end:Colors.green), weight: 1.0),
+    
+
+  ]
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Column(
-        children: [
+      backgroundColor: Color(0xffffeec3),
+      body: SafeArea(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: Lottie.network(
 
-        ],
+                  "https://lottie.host/0700d1eb-386a-439c-9575-972bc68be6d7/RImRjs8p8D.json",
+                  width: screenWidth,
+                  height: screenHeight /4,
+                  fit: BoxFit.cover,
+                repeat: true,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
