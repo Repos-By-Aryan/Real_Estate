510a53cd02d4398d1d9acb1b541d4ef935a0e852
Aryan Kumar <aryankumawat01@gmail.com>
2023-10-14 23:51:43 +0530

    Firestore Configured

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,6 +1,7 @@
-// ignore_for_file: prefer_const_constructors
 
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:real_estate/constants/constants.dart';
 
@@ -17,6 +18,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
     final screenHeight = MediaQuery.of(context).size.height;
+    final firestore = FirebaseFirestore.instance.collection('listings');
     bool isCardSelected = false;
 
     return Container(
@@ -281,7 +283,82 @@ class _HomeScreenState extends State<HomeScreen> {
                         Text('Featured Estates',style: subheading,),
                       TextButton(onPressed: (){}, child: Text('View all',style:text)),
                     ],
-                  )
+                  ),
+                  StreamBuilder(stream: firestore, builder:(BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot){
+                    if(snapshot.connectionState == ConnectionState.waiting){
+                      return const Center(child:CircularProgressIndicator());
+                    }
+                    if(snapshot.hasError){
+                      return const Text("An error occured.");
+                    }
+                    return SizedBox(
+                      height: 180,
+                      width: screenWidth,
+                      child: ListView.builder(
+                          shrinkWrap: true,
+                          scrollDirection: Axis.horizontal,
+                          itemCount: 2,
+                          itemBuilder: (context, index) {
+                            return Stack(
+                              fit: StackFit.passthrough,
+                              children: [
+                                Container(
+                                  margin: EdgeInsets.symmetric(horizontal: 7),
+                                  width:screenWidth*0.8,
+                                  decoration:BoxDecoration(
+                                      image: DecorationImage(image: AssetImage('assets/images/one.webp'),fit: BoxFit.cover,),
+                                      borderRadius: BorderRadius.only(topLeft: Radius.circular(15),topRight: Radius.circular(15),bottomRight: Radius.circular(15))
+                                  ),
+                                ),
+                                Positioned(
+                                  top:20,
+                                  left:20,
+                                  child:  Text("Halloween",style: TextStyle(
+                                    fontFamily: 'Lato',
+                                    fontWeight: FontWeight.w700,
+                                    color: Colors.white,
+                                    fontSize: 30,
+                                  ),),
+                                ),
+
+                                Positioned(
+                                  top:50,
+                                  left:20,
+                                  child:  Text("Sale!",style: TextStyle(
+                                    fontFamily: 'Lato',
+                                    fontWeight: FontWeight.w700,
+                                    color: Colors.white,
+                                    fontSize: 30,
+                                  ),),
+                                ),
+                                Positioned(
+                                  top:90,
+                                  left:20,
+                                  child:  Text("All discounts upto 60%",style: TextStyle(
+                                    fontFamily: 'Lato',
+                                    color: Colors.white,
+                                    fontSize: 15,
+                                  ),),
+                                ),
+                                Positioned(
+                                  bottom:0,
+                                  left:7,
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      color: Color(0xff234F68),
+                                      borderRadius: BorderRadius.only(topRight: Radius.circular(18),),
+                                    ),
+                                    child: Padding(
+                                      padding: const EdgeInsets.symmetric(horizontal: 25.0,vertical: 7),
+                                      child: Center(child:Icon(Icons.arrow_right_alt,color: Colors.white,size:30,)),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            );
+                          }),
+                    );
+                  }),
                 ],
               ),
             ),
