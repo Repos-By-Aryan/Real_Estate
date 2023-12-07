250e4cce42f6c5bc35c55dc9ee22852e722cec9e
Aryan Kumar <aryankumawat01@gmail.com>
2023-12-08 00:01:15 +0530

    main commit

--- lib/home/property_detail.dart
+++ lib/home/property_detail.dart
@@ -1,221 +1,239 @@
-import 'package:carousel_slider/carousel_slider.dart';
+import 'package:banner_carousel/banner_carousel.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 import '../constants/constants.dart';
 bool active = false;
+
 class PropertyDetail extends StatefulWidget {
-  const PropertyDetail({super.key});
+  final data;
+  const PropertyDetail({super.key,required this.data});
 
   @override
   State<PropertyDetail> createState() => _PropertyDetailState();
 }
 int currentIndex= 0;
+List imageUrls =[];
+
+
 class _PropertyDetailState extends State<PropertyDetail> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
     final screenHeight = MediaQuery.of(context).size.height;
     final firestore =
-    FirebaseFirestore.instance.collection('listings').snapshots();
-    final fi
-    return Container(
-    width: screenWidth,
-    height: screenHeight,
-    constraints: const BoxConstraints.expand(),
-    child: Scaffold(
+        FirebaseFirestore.instance.collection('listings').snapshots();
 
-      body: SafeArea(
-        minimum: const EdgeInsets.all(10.0),
-        child: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: screenHeight * 0.01,
-              ),
+    // final property = FirebaseFirestore.instance.collection('listings').doc(widget.data['id']);
 
-              Stack(
-                children:[
-                  Image.network(),
-                  Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xffF5F4F8),
-                        elevation: 0.2,
-                        shape: const CircleBorder(),
-                        padding: const EdgeInsets.all(12),
-                      ),
-                      child: const Icon(
-                        Icons.chevron_left,
-                        size: 23,
-                        color: Colors.black87,
-                      ),
-                    ),
-                    Row(
+    return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+        stream: firestore,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return CircularProgressIndicator(); // or a loading indicator
+          }
+
+          if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          }
+
+          // Clear the existing list
+          List<BannerModel> bannerImage = [];
+          var desiredDocument = snapshot.data!.docs.firstWhere(
+            (doc) => doc.id == widget.data['id'],
+          );
+          // Populate the list with data from Firestore
+          if (desiredDocument != null) {
+            var imageArray = List<String>.from(
+                desiredDocument['image_urls']); // Retrieve the image_urls array
+            var id = desiredDocument.id; // Use doc.id to get the document ID
+
+            for (var imagePath in imageArray) {
+              bannerImage.add(BannerModel(imagePath: imagePath, id: id));
+            }
+
+            return Container(
+              width: screenWidth,
+              height: screenHeight,
+              constraints: const BoxConstraints.expand(),
+              child: Scaffold(
+                body: SafeArea(
+                  minimum: const EdgeInsets.all(10.0),
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.vertical,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: const Color(0xcbffffff),
-                            elevation: 0.2,
-                            shape: const CircleBorder(),
-                            padding: const EdgeInsets.all(12),
-                          ),
-                          child: const Icon(
-                            Icons.file_upload_outlined,
-                            size: 25,
-                            color: Colors.black87,
-                          ),
+                        SizedBox(
+                          height: screenHeight * 0.01,
                         ),
-                        ElevatedButton(
-                          onPressed: () {
-                            setState(() {
-                              active=!active;
-                            });
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: active?const Color(0xff8ac73f):const Color(0xcbffffff),
-                            elevation: 0.2,
-                            shape: const CircleBorder(),
-                            padding: const EdgeInsets.all(12),
-                          ),
-                          child: active?SvgPicture.asset('assets/svg/heart_w.svg',width: 25,):Image.asset('assets/images/heart.png',width: 25),
+                        Stack(
+                          children: [
+                            BannerCarousel.fullScreen(
+                              banners: bannerImage,
+                              customizedIndicators: IndicatorModel.animation(
+                                  width: 20,
+                                  height: 5,
+                                  spaceBetween: 2,
+                                  widthAnimation: 50),
+                              height: screenHeight * 0.5,
+                              activeColor: theme,
+                              disableColor: Colors.white,
+                              animation: true,
+                              borderRadius: screenHeight * 0.05,
+                              indicatorBottom: false,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                ElevatedButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: const Color(0xffF5F4F8),
+                                    elevation: 0.2,
+                                    shape: const CircleBorder(),
+                                    padding: const EdgeInsets.all(12),
+                                  ),
+                                  child: const Icon(
+                                    Icons.chevron_left,
+                                    size: 23,
+                                    color: Colors.black87,
+                                  ),
+                                ),
+                                Row(
+                                  children: [
+                                    ElevatedButton(
+                                      onPressed: () {},
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor:
+                                            const Color(0xcbffffff),
+                                        elevation: 0.2,
+                                        shape: const CircleBorder(),
+                                        padding: const EdgeInsets.all(12),
+                                      ),
+                                      child: const Icon(
+                                        Icons.file_upload_outlined,
+                                        size: 25,
+                                        color: Colors.black87,
+                                      ),
+                                    ),
+                                    ElevatedButton(
+                                      onPressed: () {
+                                        setState(() {
+                                          active = !active;
+                                        });
+                                      },
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor: active
+                                            ? const Color(0xff8ac73f)
+                                            : const Color(0xcbffffff),
+                                        elevation: 0.2,
+                                        shape: const CircleBorder(),
+                                        padding: const EdgeInsets.all(12),
+                                      ),
+                                      child: active
+                                          ? SvgPicture.asset(
+                                              'assets/svg/heart_w.svg',
+                                              width: 25,
+                                            )
+                                          : Image.asset(
+                                              'assets/images/heart.png',
+                                              width: 25),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                            Positioned(
+                              bottom: 18,
+                              left: 100,
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: const Color(0xff234F68),
+                                  borderRadius: BorderRadius.circular(30),
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                    horizontal: 16.0,
+                                    vertical: 10,
+                                  ),
+                                  child: Text(
+                                    desiredDocument.get('category').toString(),
+                                    style: const TextStyle(
+                                      fontFamily: 'Lato',
+                                      color: Colors.white,
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Positioned(
+                              bottom: 18,
+                              left: 18,
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: const Color(0xff234F68),
+                                  borderRadius: BorderRadius.circular(30),
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                    horizontal: 16.0,
+                                    vertical: 10,
+                                  ),
+                                  child: Row(
+                                    children: [
+                                      SizedBox(
+                                        child: SvgPicture.asset(
+                                          'assets/svg/star.svg',
+                                          width: 20,
+                                          height: 20,
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.only(left: 4),
+                                        child: Text(
+                                          desiredDocument
+                                              .get('rating')
+                                              .toString(),
+                                          style: const TextStyle(
+                                            fontFamily: 'Lato',
+                                            color: Colors.white,
+                                            fontSize: 14,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
+                        SizedBox(
+                          height: screenHeight * 0.04,
+                        ),
+                        Row(),
                       ],
                     ),
-                  ],
+                  ),
                 ),
-
-                ],
               ),
-              SizedBox(
-                height: screenHeight * 0.04,
-              ),
-
-            ],
-          ),
-        ),
-      ),
-    ),
-    );
+            );
+          }
+          return Container();
+        });
   }
 }
-// import 'dart:io';
-//
-// import 'package:flutter/foundation.dart';
-// import 'package:flutter/material.dart';
-// import 'package:image_picker/image_picker.dart';
-//
-// void main() {
-//   runApp(const MyApp());
-// }
-//
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       theme: ThemeData(primaryColor: Colors.green),
-//       home: const PropertyDetail(),
-//       debugShowCheckedModeBanner: false,
-//     );
-//   }
-// }
-//
-// class PropertyDetail extends StatefulWidget {
-//   const PropertyDetail({super.key});
-//
-//   @override
-//   State<PropertyDetail> createState() => _PropertyDetailState();
-// }
-//
-// class _PropertyDetailState extends State<PropertyDetail> {
-//   List<File> selectedImages = [];
-//   final picker = ImagePicker();
-//   @override
-//   Widget build(BuildContext context) {
-//     // display image selected from gallery
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Multiple Images Select'),
-//         backgroundColor: Colors.green,
-//         actions: const [],
-//       ),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             const SizedBox(
-//               height: 20,
-//             ),
-//             ElevatedButton(
-//               style: ButtonStyle(
-//                   backgroundColor: MaterialStateProperty.all(Colors.green)),
-//               child: const Text('Select Image from Gallery and Camera'),
-//               onPressed: () {
-//                 getImages();
-//               },
-//             ),
-//             const Padding(
-//               padding: EdgeInsets.symmetric(vertical: 18.0),
-//               child: Text(
-//                 "GFG",
-//                 textScaleFactor: 3,
-//                 style: TextStyle(color: Colors.green),
-//               ),
-//             ),
-//             Expanded(
-//               child: SizedBox(
-//                 width: 300.0,
-//                 child: selectedImages.isEmpty
-//                     ? const Center(child: Text('Sorry nothing selected!!'))
-//                     : GridView.builder(
-//                   itemCount: selectedImages.length,
-//                   gridDelegate:
-//                   const SliverGridDelegateWithFixedCrossAxisCount(
-//                       crossAxisCount: 3),
-//                   itemBuilder: (BuildContext context, int index) {
-//                     return Center(
-//                         child: kIsWeb
-//                             ? Image.network(selectedImages[index].path)
-//                             : Image.file(selectedImages[index]));
-//                   },
-//                 ),
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-//
-//   Future getImages() async {
-//     final pickedFile = await picker.pickMultiImage(
-//         imageQuality: 100, maxHeight: 1000, maxWidth: 1000);
-//     List<XFile> xfilePick = pickedFile;
-//
-//     setState(
-//           () {
-//         if (xfilePick.isNotEmpty) {
-//           for (var i = 0; i < xfilePick.length; i++) {
-//             selectedImages.add(File(xfilePick[i].path));
-//           }
-//         } else {
-//           ScaffoldMessenger.of(context).showSnackBar(
-//               const SnackBar(content: Text('Nothing is selected')));
-//         }
-//       },
-//     );
-//   }
-// }
-//
+
